29dc7a5d924d0b00b5e33a1038488afb2809a71d
Mawuli Adzoe <mawuli738@revature.net>
2024-08-05 14:25:18 -0700

    Initial server structure

---
+++ server/RevShop/src/test/java/net/revature/RevShop/RevShopApplicationTests.java
@@ -0,0 +1,13 @@
+package net.revature.RevShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
