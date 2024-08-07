b5bcdff99b73220639766341c3ab1692afc2f13f
Jack <jiangjiang913@revature.net>
2024-08-07 12:18:55 -0400

    Renamed `index.js` & `App.js` to `.tsx`

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
