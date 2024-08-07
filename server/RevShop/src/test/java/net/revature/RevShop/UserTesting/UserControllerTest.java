fd485c210edb7f4bea4b83bb1aec0811af462632
waiyat.hamdani <waiyat.hamdani@fdmgroup.com>
2024-08-07 15:32:29 -0400

    just put this so mawi can tested

--- server/RevShop/src/test/java/net/revature/RevShop/UserTesting/UserControllerTest.java
+++ server/RevShop/src/test/java/net/revature/RevShop/UserTesting/UserControllerTest.java
@@ -9,14 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
@@ -30,6 +33,7 @@ public class UserControllerTest {
 
     private User user;
     private Long MyIdL = 1L;
+    private MockMvc mockMvc ;
 
     @BeforeEach
     public void setUp() {
@@ -43,21 +47,15 @@ public class UserControllerTest {
     //testing
     @Test
     public void testRegisterUser() throws Exception {
-        when(userService.createUser(any(User.class))).thenReturn(user);
         when(userService.findUserByUsername("YuQi")).thenReturn(null);
+        when(userService.createUser(any(User.class))).thenReturn(user);
 
-        ResponseEntity<User> response = userController.createUser(user);
-
-        assertEquals(200, response.getStatusCodeValue());
-        assertEquals("YuQi", response.getBody().getUsername());
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"username\": \"YuQi\", \"password\": \"Freak\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("YuQi"));
     }
 
-    @Test
-    public void testRegisterUserAlreadyExists() {
-        when(userService.findUserByUsername("YuQi")).thenReturn(user);
 
-        ResponseEntity<User> response = userController.createUser(user);
-
-        assertEquals(400, response.getStatusCodeValue());
-    }
 }
