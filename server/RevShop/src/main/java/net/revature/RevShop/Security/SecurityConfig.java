a6c0e74aca6c11a3c504937e94dc9f2f726b4344
Finw7 <finwall26@gmail.com>
2024-08-12 18:31:51 -0400

    Security

--- server/RevShop/src/main/java/net/revature/RevShop/Security/SecurityConfig.java
+++ server/RevShop/src/main/java/net/revature/RevShop/Security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth", "/api/users").permitAll()
                         .requestMatchers("/products/**").permitAll()
+                        .requestMatchers("/api/cart/add").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
