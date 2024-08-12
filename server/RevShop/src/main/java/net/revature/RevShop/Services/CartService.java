0e6aad52f90f6e0f6bf4ddf1fd6d6040f74fbbe2
Finw7 <finwall26@gmail.com>
2024-08-12 18:30:56 -0400

    Service

---
+++ server/RevShop/src/main/java/net/revature/RevShop/Services/CartService.java
@@ -0,0 +1,41 @@
+package net.revature.RevShop.Services;
+
+import net.revature.RevShop.Models.CartItem;
+import net.revature.RevShop.Models.Product;
+import net.revature.RevShop.Models.User;
+import net.revature.RevShop.Repositories.CartRepository;
+import net.revature.RevShop.Repositories.ProductRepository;
+import net.revature.RevShop.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+
+    private final CartRepository cartRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public CartService(CartRepository cartRepository, UserRepository userRepository, ProductRepository productRepository) {
+        this.cartRepository = cartRepository;
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
+    }
+
+    public CartItem addProductToCart(Integer buyerId, Integer productId, Integer quantity) {
+        User buyer = userRepository.findById(buyerId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+
+        CartItem cartItem = new CartItem();
+        cartItem.setBuyer(buyer);
+        cartItem.setSeller(product.getSeller());
+        cartItem.setProduct(product);
+        cartItem.setQuantity(quantity);
+
+        return cartRepository.save(cartItem);
+    }
+}
