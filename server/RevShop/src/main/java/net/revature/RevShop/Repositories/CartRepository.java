c9755752d70f4e8d176dd0cad1cabc7eb578f156
Finw7 <finwall26@gmail.com>
2024-08-12 18:30:11 -0400

    Repository

---
+++ server/RevShop/src/main/java/net/revature/RevShop/Repositories/CartRepository.java
@@ -0,0 +1,13 @@
+package net.revature.RevShop.Repositories;
+
+import net.revature.RevShop.Models.CartItem;
+import net.revature.RevShop.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartRepository extends JpaRepository<CartItem, Integer> {
+    List<CartItem> findByBuyer(User buyer);
+}
