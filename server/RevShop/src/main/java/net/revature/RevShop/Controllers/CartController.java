81468458ec8ff5a21608a2edc1b4226607c9d504
Finw7 <finwall26@gmail.com>
2024-08-13 17:44:39 -0400

    Controller update

--- server/RevShop/src/main/java/net/revature/RevShop/Controllers/CartController.java
+++ server/RevShop/src/main/java/net/revature/RevShop/Controllers/CartController.java
@@ -3,8 +3,11 @@ package net.revature.RevShop.Controllers;
 import net.revature.RevShop.Models.CartItem;
 import net.revature.RevShop.Services.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -22,5 +25,16 @@ public class CartController {
     public ResponseEntity<CartItem> addProductToCart(@RequestParam Integer buyerId, @RequestParam Integer productId, @RequestParam Integer quantity) {
         CartItem cartItem = cartService.addProductToCart(buyerId, productId, quantity);
         return ResponseEntity.ok(cartItem);
+    }
+    @GetMapping("/view")
+    @ResponseStatus(HttpStatus.OK)
+    public List<CartItem> viewCart(){
+        return cartService.viewCart();
+    }
+
+    @DeleteMapping("/delete/{cartItemId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeProductFromCart(@PathVariable("cartItemId")Integer cartItemId){
+        cartService.removeProductFromCart(cartItemId);
     }
 }
